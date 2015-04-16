dc415facea843cf6414879abe98b6458339196b9
x14g008 <x14g008@PC505N008.chiba-fjb.ac.jp>
2015-04-16 15:01:16 +0900

    test

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		int[] a;
+		a = new int[3];
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+
+		System.out.println(a.length);
+	
+	}
+	}
+/*
+ * 
+ * 
+ * 
+ */
+
